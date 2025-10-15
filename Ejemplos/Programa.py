48fd64241cdb863ee454141e3dabff7737d62982
avllgrn <avillagran@ipn.mx>
2025-10-15 08:37:24 -0600

    Contador desde 0, hasta infinito, de uno en uno

--- Ejemplos/Programa.py
+++ Ejemplos/Programa.py
@@ -3,6 +3,7 @@ import os
 if __name__ == '__main__':
     os.system('cls')
 
-    i = 1
+    i = 0
     while True:                 # Mientras True sea verdadero
         print(f' {i} ')         # Se pinta el valor de la variable
+        i = i+1                 # Con cada vuelta del ciclo, la variable incrementa su valor en uno
