edaf4e12d5a1dd9157231eebe340d5e29781144d
avllgrn <avillagran@ipn.mx>
2025-10-15 08:32:46 -0600

    Ciclo infinito con una "variable" (que no cambia de valor)

--- Ejemplos/Programa.py
+++ Ejemplos/Programa.py
@@ -3,5 +3,6 @@ import os
 if __name__ == '__main__':
     os.system('cls')
 
-    while True:         # Mientras True sea verdadero
-        print('1')      # Se pinta uno
+    i = 1
+    while True:                 # Mientras True sea verdadero
+        print(f' {i} ')         # Se pinta el valor de la variable
