7dafa4edffb59a3438c10436714d57f0daa30f0b
avllgrn <avillagran@ipn.mx>
2025-10-15 08:54:45 -0600

    Contadores con funciones

--- Ejemplos/Programa.py
+++ Ejemplos/Programa.py
@@ -1,13 +1,35 @@
 import os
 
+def contadorAscendente(ini, fin, inc):
+    i = ini                     # Valor inicial de la variable contador
+    while i < fin:              # Mientras el valor de i sea menor al fin,
+        print(f' {i} ')         # Se pinta el valor de la variable
+        i = i+inc               # Con cada vuelta del ciclo, la variable incrementa su valor en inc
+
+def contadorDescendente(ini, fin, dec):
+    i = ini                     # Valor inicial de la variable contador
+    while i > fin:              # Mientras el valor de i sea mayor al fin,
+        print(f' {i} ')         # Se pinta el valor de la variable
+        i = i-dec               # Con cada vuelta del ciclo, la variable decrementa su valor en dec
+
 if __name__ == '__main__':
     os.system('cls')
 
     ini = float(input('Ingresa inicio del contador '))
     fin = float(input('Ingresa fin del contador '))
-    dec = float(input('Ingresa decremento del contador '))
+    
+    if ini < fin:
+        inc = float(input('Ingresa incremento del contador '))
 
-    i = ini                     # Valor inicial de la variable contador
-    while i > fin:              # Mientras el valor de i sea mayor al fin,
-        print(f' {i} ')         # Se pinta el valor de la variable
-        i = i-dec               # Con cada vuelta del ciclo, la variable decrementa su valor en dec
+        if inc>0:                               # Si el incremento es positivo, 
+            contadorAscendente(ini, fin, inc)   # se invoca la funcion que cuenta
+        else:
+            print('Error! Se genera ciclo infinito')
+
+    else:
+        dec = float(input('Ingresa decremento del contador '))
+
+        if dec>0:                               # Si el decremento es positivo, 
+            contadorDescendente(ini, fin, dec)   # se invoca la funcion que cuenta
+        else:
+            print('Error! Se genera ciclo infinito')
