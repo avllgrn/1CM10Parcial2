ba9f38ab6b0963845224c958e1b1501cf00f029d
avllgrn <avillagran@ipn.mx>
2025-10-15 08:16:29 -0600

    Menu infinito (termina con break)

--- Ejemplos/Programa.py
+++ Ejemplos/Programa.py
@@ -1,7 +1,49 @@
 import os
+import math
+
+def gradosAradianes(grados):
+    return grados * math.pi / 180
+
+def funcion1():
+    grados = float(input('Ingresa grados '))
+    radianes = gradosAradianes(grados)
+    print(f'{grados}° = {radianes} rad')
+
+def fDeXcuadrada(x):
+    return x**2
+
+def funcion2():
+    x = float(input('Ingresa x '))
+    y = fDeXcuadrada(x)
+    print(f'f({x}) = {y}')
+
+def selecciona(opcion):
+    match opcion:
+        case '1':
+            funcion1()
+        case '2':
+            funcion2()
+        case '3':
+            print('Adiós! =)')
+        case _:
+            print('Opción invalida... =(')
+
+def menu():
+    os.system('cls')
+    print('1. Grados a radianes')
+    print('2. f(x) = x^2')
+    print('3. Salir')
+    opcion = input('¿Cuál es tu opción? ')
+    os.system('cls')
+    selecciona(opcion)
+    return opcion
 
 if __name__ == '__main__':
     os.system('cls')
 
-    while 1 < 2:
-        print('Este mensaje se repite mientras uno sea menor que dos')
+    while True:             # Ciclo infinito, mientras True sea verdadero
+
+        if menu() == '3':   # Si la funcion menu retorna tres, se rompe/termina el ciclo infinito;
+            break
+        else:               # si no, se pide un teclazo y continúa el ciclo infinito 
+            input('\n\nPresiona una tecla para continuar...')
