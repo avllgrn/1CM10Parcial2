21e665cd4e6846edea5d819be41ce071e110f745
avllgrn <avillagran@ipn.mx>
2025-10-15 08:03:28 -0600

    Ciclo infinito (otro)

--- Ejemplos/Programa.py
+++ Ejemplos/Programa.py
@@ -3,5 +3,5 @@ import os
 if __name__ == '__main__':
     os.system('cls')
 
-    while True:
-        print('Este mensaje se repite mientras True sea verdadero')
+    while 1 < 2:
+        print('Este mensaje se repite mientras uno sea menor que dos')
