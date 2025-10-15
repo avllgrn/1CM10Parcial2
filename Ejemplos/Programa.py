c6f27bf7af956dc8d0a78d3124acafd642be78e4
avllgrn <avillagran@ipn.mx>
2025-10-15 08:43:31 -0600

    Contador desde diez, hasta cero, de uno en uno [10, 0]

--- Ejemplos/Programa.py
+++ Ejemplos/Programa.py
@@ -4,6 +4,6 @@ if __name__ == '__main__':
     os.system('cls')
 
     i = 10
-    while i > 0:                # Mientras el valor de i sea mayor a cero,
+    while i >= 0:               # Mientras el valor de i sea mayor o igual a cero,
         print(f' {i} ')         # Se pinta el valor de la variable
         i = i-1                 # Con cada vuelta del ciclo, la variable decrementa su valor en uno
