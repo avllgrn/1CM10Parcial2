1dc93b363e96345dd193eb3ec962165f8f1f8ef8
avllgrn <avillagran@ipn.mx>
2025-10-23 11:16:04 -0600

    Funcion de Fibonacci

--- Ejemplos/Programa.py
+++ Ejemplos/Programa.py
@@ -1,26 +1,19 @@
 import os
-import math
 
-def serieFibonacci(n):
-    s = 0       # La suma acumulada DEBE comenzar en cero
-
+def fibonacci(n):
     a = 0
     b = 1
 
-    if n>=0 and n<=1:
-        print(f'{a}')
-        if n==1:
-            print(f'{b}')
+    if n==0 or n==1:
+        return n
     elif n>1:
-        print(f'{a}')
-        print(f'{b}')
         i=2         # Contador en rango [2, n]
         while i<=n:
             c = a+b
-            print(f'{c}')
             a=b
             b=c
             i = i+1
+        return c
 
 if __name__ == '__main__':
     os.system('cls')
@@ -28,5 +21,6 @@ if __name__ == '__main__':
     n = int(input('Ingresa n '))
     print()
 
-    serieFibonacci(n)
+    f = fibonacci(n)
+    print(f'fibonacci({n}) = {f}')
     print()
