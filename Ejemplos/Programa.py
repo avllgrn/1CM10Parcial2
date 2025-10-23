e2407cf4538dae265c11381086917ca32ccff548
avllgrn <avillagran@ipn.mx>
2025-10-23 11:06:09 -0600

    Serie e^x

--- Ejemplos/Programa.py
+++ Ejemplos/Programa.py
@@ -1,4 +1,5 @@
 import os
+import math
 
 def factorial(n):
     f = 1       # La multiplicación acumulada DEBE comenzar en uno
@@ -10,11 +11,24 @@ def factorial(n):
         i = i+1 # De uno en uno
     return f
 
+def serieE(x, n):
+    s = 0
+
+    i = 0
+    while i<=n:
+        # print(f'i = {i}\te^{x} = {s}')
+        s = s + math.pow(x, i)/factorial(i)
+        i = i+1
+    return s
+
 if __name__ == '__main__':
     os.system('cls')
 
+    x = float(input('Ingresa x '))
     n = int(input('Ingresa n '))
+    print()
 
-    f = factorial(n)
+    e = serieE(x, n)
 
-    print(f'{n}! = {f}')
+    print()
+    print(f'e^{x}! = {e}')
