a1040847c669c3e1c502b30d7f4f5b4a4768b5f7
avllgrn <avillagran@ipn.mx>
2025-10-23 10:55:56 -0600

    Suma acumulada en rango [0, n)

--- Ejemplos/Programa.py
+++ Ejemplos/Programa.py
@@ -1,43 +1,20 @@
 import os
-import math
-import matplotlib.pyplot
 
-def gradosAradianes(grados):
-    return grados * math.pi / 180
+def sumaDesdeCeroHasta(n):
+    s = 0       # La suma acumulada DEBE comenzar en cero
 
-def fDeXseno(xGrados):
-    xRadianes = gradosAradianes(xGrados)                    # Se convierte a radianes los grados recibidos
-    return math.sin(xRadianes)                              # Se contesta el seno de los radianes convertidos
+    i = 0       
+    while i<=n: # Contador en rango [0, n)
+        s = s+i
 
-def grafica():
-    ini = float(input('Ingresa inicio de la gráfica '))
-    fin = float(input('Ingresa fin de la gráfica '))
-    inc = math.fabs( float(input('Ingresa incremento de la tabla ')) )
-
-    if inc!=0:                                              # Si hay incremento, 
-
-        X = []                                              # Se define una lista vacía, para los valores que tomará x
-        Y = []                                              # Se define una lista vacía, para los valores que tomará y
-
-        # Contador para generar los valores de la gráfica
-        i = ini                                             # Se inicializa el contador
-        while i<fin:                                        # Se condiciona el final del contador
-
-            X.append( i )                                   # Se agrega el valor actual del contador i,                      a la lista X
-            Y.append( fDeXseno(i) )                         # Se agrega el valor actual del contador i evaluado en fDeXseno, a la lista Y 
-
-            i = i+inc                                       # Se incrementa el contador
-
-
-        # Después de generar los valores,
-        matplotlib.pyplot.plot(X,Y)                         # Se genera la gráfica (en memoria)
-        matplotlib.pyplot.show()                            # Se muestra la gráfica
-
-    else:
-        print('Error! Se genera ciclo infinito')
-
+        i = i+1 # De uno en uno
+    return s
 
 if __name__ == '__main__':
     os.system('cls')
 
-    grafica()
+    n = int(input('Ingresa n '))
+
+    s = sumaDesdeCeroHasta(n)
+
+    print(f's = {s}')
