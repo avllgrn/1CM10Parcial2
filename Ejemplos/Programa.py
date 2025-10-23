41dece223bae9c882d8214efb5ba949f272f0cc9
avllgrn <avillagran@ipn.mx>
2025-10-23 11:11:19 -0600

    Serie de Fibonacci

--- Ejemplos/Programa.py
+++ Ejemplos/Programa.py
@@ -1,34 +1,32 @@
 import os
 import math
 
-def factorial(n):
-    f = 1       # La multiplicación acumulada DEBE comenzar en uno
+def serieFibonacci(n):
+    s = 0       # La suma acumulada DEBE comenzar en cero
 
-    i = 1
-    while i<=n: # Contador en rango [1, fin]
-        f = f*i
+    a = 0
+    b = 1
 
-        i = i+1 # De uno en uno
-    return f
-
-def serieE(x, n):
-    s = 0
-
-    i = 0
-    while i<=n:
-        # print(f'i = {i}\te^{x} = {s}')
-        s = s + math.pow(x, i)/factorial(i)
-        i = i+1
-    return s
+    if n>=0 and n<=1:
+        print(f'{a}')
+        if n==1:
+            print(f'{b}')
+    elif n>1:
+        print(f'{a}')
+        print(f'{b}')
+        i=2         # Contador en rango [2, n]
+        while i<=n:
+            c = a+b
+            print(f'{c}')
+            a=b
+            b=c
+            i = i+1
 
 if __name__ == '__main__':
     os.system('cls')
 
-    x = float(input('Ingresa x '))
     n = int(input('Ingresa n '))
     print()
 
-    e = serieE(x, n)
-
+    serieFibonacci(n)
     print()
-    print(f'e^{x}! = {e}')
