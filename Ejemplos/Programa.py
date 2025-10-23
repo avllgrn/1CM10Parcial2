401840ca14e39955776d9dd7f3f49b8c56a99be0
avllgrn <avillagran@ipn.mx>
2025-10-23 10:58:59 -0600

    Suma acumulada en rango [ini, fin)

--- Ejemplos/Programa.py
+++ Ejemplos/Programa.py
@@ -1,10 +1,10 @@
 import os
 
-def sumaDesdeCeroHasta(n):
+def sumaDesdeIniHastaFin(ini, fin):
     s = 0       # La suma acumulada DEBE comenzar en cero
 
-    i = 0       
-    while i<=n: # Contador en rango [0, n)
+    i = ini
+    while i<=fin: # Contador en rango [ini, fin)
         s = s+i
 
         i = i+1 # De uno en uno
@@ -13,8 +13,9 @@ def sumaDesdeCeroHasta(n):
 if __name__ == '__main__':
     os.system('cls')
 
-    n = int(input('Ingresa n '))
+    ini = int(input('Ingresa ini '))
+    fin = int(input('Ingresa fin '))
 
-    s = sumaDesdeCeroHasta(n)
+    s = sumaDesdeIniHastaFin(ini, fin)
 
     print(f's = {s}')
